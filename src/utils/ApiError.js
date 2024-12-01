724eb681444bfe33d51d5f11342190bc7ca8d5db
yatin Gaur <yatingaur66@gmail.com>
2024-12-01 11:24:27 +0530

    custom api and error handling

---
+++ src/utils/ApiError.js
@@ -0,0 +1,16 @@
+class ApiErrror extends Error{
+    constructor(statusCode,errors=[],message="Something went wrong",stack=""){
+        super(message)
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success=false
+        this.errors=errors
+
+        if(stack){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
