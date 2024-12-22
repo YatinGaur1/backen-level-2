caa4791ff4e3af3f8bca69ddbdaf0f21ebb95290
yatin Gaur <yatingaur66@gmail.com>
2024-12-22 22:11:39 +0530

    add multer

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -50,7 +50,7 @@ const userSchema=new Schema({
 }
 )
 userSchema.pre("save",async function (next) {
-    if(!this.isModified("password")) return next();
+    if(this.isModified("password")) return next();
 
     this.password=bcrypt.hash(this.password,10)//mean 10 round of decrpt process run
     next();
--- src/utils/fileupload.cloudinary.js
+++ src/utils/fileupload.cloudinary.js
@@ -2,7 +2,22 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs"
 
 cloudinary.config({ 
-    cloud_name: 'yatingaur', 
-    api_key: '484512967771445', 
-    api_secret: '3Gi50IVZb_fZq-FL0-SwxOkCzRA'
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:  process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
+
+const uploadOnCloudinary=async (localFilePath) => {
+    try {
+        if(!localFilePath)return null
+        //upload file on cloudinary
+       const response=await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+        //file has been uploaded successfully
+        console.log("file is uploaded on cloudinary",response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)//remove the locally saved temporary file as the upload operation got failed
+        return error;
+    }
+}
+export {uploadOnCloudinary};
