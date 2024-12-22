6bfe1ccd6bfe9ced1164e67b9abddff208352bea
yatin Gaur <yatingaur66@gmail.com>
2024-12-22 21:00:49 +0530

    add evn

--- src/utils/fileupload.cloudinary.js
+++ src/utils/fileupload.cloudinary.js
@@ -1,1 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({ 
+    cloud_name: 'yatingaur', 
+    api_key: '484512967771445', 
+    api_secret: '3Gi50IVZb_fZq-FL0-SwxOkCzRA'
+});
